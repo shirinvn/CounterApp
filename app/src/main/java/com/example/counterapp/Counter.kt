1f10f4622c5aef0458b7bf96efb75db5a8b37e5b
shirinvatani <shirinvatani97@gmail.com>
2023-12-06 18:26:06 +0330

    counter ok

--- app/src/main/java/com/example/counterapp/Counter.kt
+++ app/src/main/java/com/example/counterapp/Counter.kt
@@ -2,18 +2,23 @@ package com.example.counterapp
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 
 const val COUNT_PREF_KEY = "count_pref_key"
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun TasbihCounter() {
     val context = LocalContext.current
@@ -25,21 +30,22 @@ fun TasbihCounter() {
     ) {
         Text(text = count.toString())
 
+
+
         Button(onClick = {
             count++
             saveCount(context, count)
             // Add vibration here for each click if needed
+            vibrateShort(context)
+            if (count % 100 == 0) {
+                vibrateLong(context)
+            }
         }) {
             Text(text = "افزودن")
         }
 
-        Button(onClick = {
-            // Show a confirmation dialog here if needed
-            count = 0
-            saveCount(context, count)
-        }) {
-            Text(text = "ریست")
-        }
+        ResetButton { count = 0 }
+
     }
 }
 
@@ -53,4 +59,58 @@ fun saveCount(context: Context, count: Int) {
     val editor: SharedPreferences.Editor = sharedPreferences.edit()
     editor.putInt(COUNT_PREF_KEY, count)
     editor.apply()
+}
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun vibrateShort(context: Context) {
+    val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
+    vibrator?.let {
+        it.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE))
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun vibrateLong(context: Context) {
+    val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
+    vibrator?.let {
+        it.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
+    }
+}
+
+
+@Composable
+fun ResetButton(resetAction: () -> Unit = { 0 }){
+    var showDialog by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(text = "ریست کردن شمارنده") },
+            text = { Text(text = "آیا مطمئنید می‌خواهید شمارنده را ریست کنید؟") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        resetAction() // اجرای عملیات ریست
+                        showDialog = false
+                    }
+                ) {
+                    Text(text = "تایید")
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = {
+                        showDialog = false
+                    }
+                ) {
+                    Text(text = "لغو")
+                }
+            }
+        )
+    }
+
+    Button(onClick = { showDialog = true }) {
+        Text(text = "ریست")
+    }
 }
